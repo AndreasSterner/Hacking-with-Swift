3b62074a6cfacf19b91d58cfe84f30835e22bf07
AndreasSterner <andreas@sterner.io>
2017-05-19 23:09:29 +0200

    meh

--- Storm Viewer - 01/Storm Viewer/Storm Viewer/DetailViewController.swift
+++ Storm Viewer - 01/Storm Viewer/Storm Viewer/DetailViewController.swift
@@ -12,6 +12,8 @@ class DetailViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     
+    var selectedImage: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
